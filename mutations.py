6cc58e6cbf8a1d018a086335b42d59119f93e9d2
Julian <jtakeshishojinaga@gmail.com>
2025-08-06 14:07:35 -0400

    Update mutations.py

--- mutations.py
+++ mutations.py
@@ -1,48 +1,12 @@
 from enum import Enum
 class Mutations(Enum): # The values as a percentage. I.E, 10.0 = 10%
     # They CANNOT have the same value!
-    leucism = 2.0 # Default: 2.0
-    piebaldism = 5.0 # Default: 5.0
-    albinism = 10.0 # Default: 10.0
-    melanism = 10.5 # Default: 10.5
+    leucism = 10.0 # Default: 10.0
+    piebaldism = 12.0 # Default: 12.0
+    albinism = 15.0 # Default: 15.0
+    melanism = 15.5 # Default: 15.5
     all = 0.0
     #test = 100.0
-
-class AllowedSpecies(Enum):
-    deinosuchus = 1
-    kaprosuchus = 2
-    dracoviper = 3
-    dominus = 4
-    golugore = 5
-    andrewsarchus = 6
-    madrehorn = 7
-    mammuthus = 8
-    cerataspida = 9
-    bonapartenykus = 10
-    citipati = 11
-    halszkaraptor = 12
-    ocepechelon = 13
-    noviana = 14
-    moraquile = 15
-    glowplume = 16
-    thalasrex = 17
-    nyctatyrannus = 18
-    ankylorhiza = 19
-    helicoprion = 20
-    megalania = 21
-    ophis = 22
-    smilodon = 23
-    griffin = 24
-    lisowicia = 25
-    dimetrodon = 26
-    diplocaulus = 27
-    salamander = 28
-    hibbertopterus = 29
-    xiphactinus = 30
-    archelon = 31
-    livyatan = 32
-    inostrancevia = 33
-    squalicorax = 34
 
 class Conditions(Enum):
     dwarfism = 0.6 # Default: 0.6
@@ -59,3 +23,16 @@ class Conditions(Enum):
     colorblindness = 18.0 # Default: 18.0
     severe_phobia = 20.0 # Default: 20.0
     #example_condition = 100.0
+
+class Illnesses(Enum):
+    Chronic_Wasting_Disease = 0.8
+    rabies = 0.9
+    haemorrhagic_fever = 1.0
+    amoebiasis = 2.0
+    anaplasmosis = 4.0
+    meningitis = 5.0
+    pneumonia = 7.0
+    influenza = 10.0
+    rain_rot = 12.0
+    the_common_cold = 25.0
+    #The_Black_Plague = 100.0
